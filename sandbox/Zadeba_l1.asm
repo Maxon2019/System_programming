d7d48249a4db0de7732d1391b312a05e2a1a8842
Максим Z <56646878+Maxon2019@users.noreply.github.com>
2024-09-07 11:46:12 +0000

    fasm sandbox push 32

--- sandbox/Zadeba_l1.asm
+++ sandbox/Zadeba_l1.asm
@@ -1,15 +1,15 @@
-format ELF64
+format ELF
 public _start
 
 msg db "Zadeba",0xA, "Maxim" ,0xA, "Alekseevich", 0xA, 0
 _start:
  ;инициализация регистров для вывода информации на экран
- mov rax, 4
- mov rbx, 1
- mov rcx, msg
- mov rdx, 27
+ mov eax, 4
+ mov ebx, 1
+ mov ecx, msg
+ mov edx, 27
  int 0x80
  ;инициализация регистров для успешного завершения работы программы
- mov rax, 1
- mov rbx, 0
+ mov eax, 1
+ mov ebx, 0
  int 0x80
